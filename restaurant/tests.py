2376bb14e401c4744b938d5d171588843462ef17
Kevin Cordano <kmcordano@gmail.com>
2024-10-20 10:31:39 -0500

    Testing

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Menu
 
-# Create your tests here.
+class MenuTest(TestCase):
+   def test_add_item(self):
+      item = Menu.objects.create(title = 'NewItemTest', price = 1.00, inventory = 1)
+      self.assertEqual(item.__str__(), "NewItemTest : 1.0")
